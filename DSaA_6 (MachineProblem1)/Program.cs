33af8efa8ac0388f57a8ed75de06f7716a968773
Kian_School <24-0203c@CCL1-10>
2025-03-13 12:38:24 +0800

    Added The main Code

--- DSaA_6 (MachineProblem1)/Program.cs
+++ DSaA_6 (MachineProblem1)/Program.cs
@@ -10,6 +10,40 @@ namespace DSaA_6__MachineProblem1_
     {
         static void Main(string[] args)
         {
+            int[][] Card = new int[5][];
+            Random rng = new Random();
+            int temp = 0;
+            for (int i = 0; i < Card.Length; i++)
+                Card[i] = new int[5];
+
+            for (int i = 0; i < Card.Length; i++)
+            {
+                for (int j = 0; j < Card[i].Length; j++)
+                {
+                    if (i == 0)
+                        Card[i][j] = rng.Next(1 + (j * 15), 16 + (j * 15));
+                    else
+                    {
+                        bool numPass = false;
+                        while (!numPass)
+                        {
+                            temp = rng.Next(1 + (j * 15), 16 + (j * 15));
+                            for (int k = 0; k < 5; k++)
+                            {
+                                if (Card[k][j] == temp)
+                                    break;
+                                if (k == 4)
+                                {
+                                    Card[i][j] = temp;
+                                    numPass = true;
+                                }
+                            }
+                        }
+                    }
+                    Console.Write($"[{Card[i][j]}]\t");
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
