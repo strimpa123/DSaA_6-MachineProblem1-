5ce7f702ac674a57361042a9f7480726ff591d4a
Kian_School <24-0203c@CCL1-10>
2025-03-13 11:28:34 +0800

    Add project files.

---
+++ DSaA_6 (MachineProblem1)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSaA_6__MachineProblem1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
